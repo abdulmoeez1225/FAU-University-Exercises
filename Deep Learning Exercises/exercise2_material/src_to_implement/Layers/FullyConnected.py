f402ba60ffa64efed96c673a6f58b5d0dd289d2c
abdulmoeez1225 <abdulmoeez1225@gmail.com>
2025-06-26 21:54:25 +0200

    task complete

--- Deep Learning Exercises/exercise2_material/src_to_implement/Layers/FullyConnected.py
+++ Deep Learning Exercises/exercise2_material/src_to_implement/Layers/FullyConnected.py
@@ -9,7 +9,7 @@ class FullyConnected(Base.BaseLayer):
         self.trainable = True
         self.input_size = input_size
         self.output_size = output_size
-        self.weights = np.random.uniform(size = (input_size, output_size))
+        self.weights = np.random.uniform(size=(input_size + 1, output_size))
         self.bias = np.random.uniform(size = (1, output_size))
         self.gradient_weights = None
         self.gradient_bias = None
@@ -27,16 +27,17 @@ class FullyConnected(Base.BaseLayer):
         self._optimizer.bias = copy.deepcopy(optimizer)
 
     def forward(self, input_tensor):
-        self.lastIn = input_tensor
-        self.lastOut = np.dot(input_tensor, self.weights) + self.bias
+        self.lastIn = np.hstack((input_tensor, np.ones((input_tensor.shape[0], 1))))
+        self.lastOut = np.dot(self.lastIn, self.weights)
         return self.lastOut
      
     def backward(self, error_tensor):
-        dx = np.dot(error_tensor, self.weights.T)
         dW = np.dot(self.lastIn.T, error_tensor)
+        dx = np.dot(error_tensor, self.weights[:-1].T)
         db = np.sum(error_tensor, axis = 0)
         if self._optimizer != None:
-            self.weights = self._optimizer.weight.calculate_update(self.weights, dW)
+            self.weights = self._optimizer.calculate_update(self.weights, dW)
+
             self.bias = self._optimizer.bias.calculate_update(self.bias, db)
        
         self.gradient_bias = error_tensor
@@ -46,8 +47,8 @@ class FullyConnected(Base.BaseLayer):
 
 
     def initialize(self, weights_initializer, bias_initializer):
-        self.weights = weights_initializer.initialize(self.weights.shape, self.input_size, self.output_size)
-        self.bias = bias_initializer.initialize(self.bias.shape, 1, self.output_size)
+        self.weights[:-1] = weights_initializer.initialize(self.weights[:-1].shape, self.input_size, self.output_size)
+        self.weights[-1:] = bias_initializer.initialize((1, self.output_size), 1, self.output_size)
 
     
     
