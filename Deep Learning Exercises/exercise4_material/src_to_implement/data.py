87ebc26f504957fbf457127cf19c2870c1254406
abdulmoeez1225 <abdulmoeez1225@gmail.com>
2025-07-29 02:30:21 +0200

    exercise 4 complete

--- Deep Learning Exercises/exercise4_material/src_to_implement/data.py
+++ Deep Learning Exercises/exercise4_material/src_to_implement/data.py
@@ -12,4 +12,49 @@ train_std = [0.16043035, 0.16043035, 0.16043035]
 
 class ChallengeDataset(Dataset):
     # TODO implement the Dataset class according to the description
-    pass
+    def __init__(self, data, mode):
+        self.data = data
+        # self.train = (mode == 'train')
+        self.mode = mode
+        TF = tv.transforms
+        self.val_transform = TF.Compose([  
+                                    TF.ToPILImage(),
+                                    TF.ToTensor(),
+                                    TF.Normalize(train_mean, train_std),
+                                    # TF.RandomHorizontalFlip(p=0.3),
+                                    # TF.RandomVerticalFlip(p=0.3)
+                                    ])
+        self.train_transform = TF.Compose([  
+                                    TF.ToPILImage(),
+                                    TF.ToTensor(),
+                                    TF.Normalize(train_mean, train_std)
+                                    # TF.RandomHorizontalFlip(p=0.3),
+                                    # TF.RandomVerticalFlip(p=0.3)
+                                    ])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        if self.mode == "val":
+            data = self.data.iloc[idx]
+            img = imread(data['filename'], as_gray=True)
+            img = gray2rgb(img)
+            label = np.array([data['crack'], data['inactive']])
+            img = self.val_transform(img)
+            return img, label
+        if self.mode == "train":
+            data = self.data.iloc[idx]
+            img = imread(data['filename'], as_gray=True)
+            img = gray2rgb(img)
+            label = np.array([data['crack'], data['inactive']])
+            img = self.train_transform(img)
+            return img, label
+
+    @property
+    def transform(self):
+        return self._transform
+    
+    @transform.setter
+    def transform(self, transform):
+        self._transform = transform
--- Deep Learning Exercises/exercise4_material/src_to_implement/trainer.py
+++ Deep Learning Exercises/exercise4_material/src_to_implement/trainer.py
@@ -1,3 +1,5 @@
+from cProfile import label
+import numpy as np
 import torch as t
 from sklearn.metrics import f1_score
 from tqdm.autonotebook import tqdm
@@ -12,10 +14,12 @@ class Trainer:
                  train_dl=None,                # Training data set
                  val_test_dl=None,             # Validation (or test) data set
                  cuda=True,                    # Whether to use the GPU
-                 early_stopping_patience=-1):  # The patience for early stopping
+                 early_stopping_patience=-1,
+                 scheduler=None):  # The patience for early stopping
         self._model = model
         self._crit = crit
         self._optim = optim
+        self._scheduler = scheduler
         self._train_dl = train_dl
         self._val_test_dl = val_test_dl
         self._cuda = cuda
@@ -27,10 +31,11 @@ class Trainer:
             self._crit = crit.cuda()
             
     def save_checkpoint(self, epoch):
-        t.save({'state_dict': self._model.state_dict()}, 'checkpoints/checkpoint_{:03d}.ckp'.format(epoch))
+        t.save({'state_dict': self._model.state_dict()}, 'checkpoints/checkpoint.ckp')
+        self.save_onnx('checkpoints/model.onnx')
     
-    def restore_checkpoint(self, epoch_n):
-        ckp = t.load('checkpoints/checkpoint_{:03d}.ckp'.format(epoch_n), 'cuda' if self._cuda else None)
+    def restore_checkpoint(self, epoch_n, path='checkpoints'):
+        ckp = t.load(f'{path}/checkpoint.ckp', 'cuda' if self._cuda else None)
         self._model.load_state_dict(ckp['state_dict'])
         
     def save_onnx(self, fn):
@@ -48,6 +53,8 @@ class Trainer:
               output_names = ['output'], # the model's output names
               dynamic_axes={'input' : {0 : 'batch_size'},    # variable lenght axes
                             'output' : {0 : 'batch_size'}})
+        if self._cuda:
+            m = self._model.cuda()
             
     def train_step(self, x, y):
         # perform following steps:
@@ -58,15 +65,29 @@ class Trainer:
         # -update weights
         # -return the loss
         #TODO
+        self._optim.zero_grad()
+        out = self._model(x)
+        loss = self._crit(out, y.float())
+        loss.backward()
+        self._optim.step()
+        if self._scheduler is not None:
+            self._scheduler.step()
+        return loss.item()
         
         
     
     def val_test_step(self, x, y):
-        
         # predict
         # propagate through the network and calculate the loss and predictions
         # return the loss and the predictions
         #TODO
+        out = self._model(x)
+        loss = self._crit(out, y.float())
+        out = out.detach().cpu().numpy()
+        pred_0 = np.array(out[:, 0] > 0.5).astype(int)
+        pred_1 = np.array(out[:, 1] > 0.5).astype(int)
+        pred = np.stack([pred_0, pred_1], axis=1)
+        return loss.item(), pred
         
     def train_epoch(self):
         # set training mode
@@ -75,6 +96,16 @@ class Trainer:
         # perform a training step
         # calculate the average loss for the epoch and return it
         #TODO
+        self._model = self._model.train()
+        avg_loss = 0
+        for x, y in self._train_dl:
+            if self._cuda:
+                x = x.cuda()
+                y = y.cuda()
+            loss = self.train_step(x, y)
+            avg_loss += loss / len(self._train_dl)
+        return avg_loss
+
     
     def val_test(self):
         # set eval mode. Some layers have different behaviors during training and testing (for example: Dropout, BatchNorm, etc.). To handle those properly, you'd want to call model.eval()
@@ -86,23 +117,62 @@ class Trainer:
         # calculate the average loss and average metrics of your choice. You might want to calculate these metrics in designated functions
         # return the loss and print the calculated metrics
         #TODO
+        self._model = self._model.eval()
+        with t.no_grad():
+            avg_loss = 0
+            preds = []
+            labels = []
+            for x, y in self._val_test_dl:
+                if self._cuda:
+                    x = x.cuda()
+                    y = y.cuda()
+                loss, pred = self.val_test_step(x, y)
+                avg_loss += loss / len(self._val_test_dl)
+                if self._cuda:
+                    y = y.cpu()
+                pred = pred
+                preds.extend(pred)
+                labels.extend(y.numpy())
+            preds, labels = np.array(preds), np.array(labels)
+            score = f1_score(labels, preds, average='micro')
+        return avg_loss, score
         
     
     def fit(self, epochs=-1):
         assert self._early_stopping_patience > 0 or epochs > 0
         # create a list for the train and validation losses, and create a counter for the epoch 
         #TODO
+        train_losses = []
+        val_losses = []
+        val_metrics = []
+        epoch_n = 0
         
         while True:
-      
             # stop by epoch number
             # train for a epoch and then calculate the loss and metrics on the validation set
             # append the losses to the respective lists
             # use the save_checkpoint function to save the model (can be restricted to epochs with improvement)
             # check whether early stopping should be performed using the early stopping criterion and stop if so
             # return the losses for both training and validation
-        #TODO
-                    
-        
-        
-        
+            #TODO
+            if epoch_n == epochs:
+                break
+            print('Epoch: %3d'%(epoch_n+1))
+            train_loss = self.train_epoch()
+            val_loss, val_metric = self.val_test()
+            
+            if len(val_losses) != 0 and val_loss < min(val_losses):
+                self.save_checkpoint(epoch_n)
+            
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
+            val_metrics.append(val_metric)
+
+            if self._early_stopping_patience > 0:
+                if len(val_losses) > self._early_stopping_patience:
+                    if val_losses[-1] > val_losses[-self._early_stopping_patience-1]:
+                        break
+            epoch_n += 1
+            print('\tTrain Loss: %.4f\tVal Loss: %.4f\tVal Metric: %.4f'%(train_loss, val_loss, val_metric))
+        return train_losses, val_losses, val_metrics
+
